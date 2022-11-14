0fb763dda46c22cb4d152b12e094f84a577a5b8b
yue_pu <yue_pu@en-japan.com>
2022-11-14 15:09:50 +0900

    add unit test

--- testCircleCI2Tests/testCircleCI2Tests.swift
+++ testCircleCI2Tests/testCircleCI2Tests.swift
@@ -12,10 +12,12 @@ final class testCircleCI2Tests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        XCTAssertEqual(1, 1)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        XCTAssertEqual(2, 2)
     }
 
     func testExample() throws {
@@ -24,6 +26,8 @@ final class testCircleCI2Tests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        XCTAssertEqual(3, 3)
     }
 
     func testPerformanceExample() throws {
